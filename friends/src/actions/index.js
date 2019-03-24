23faa40a958662040883ba64f59b1d7a7610c5a9
Aman <amsingh714@gmail.com>
2019-03-24 17:25:39 -0400

    added history and updated actions & reducers

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 
+import { history } from "../helpers/history";
+
 export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 
-
 export const login = credentials => dispatch => {
-  dispatch({ type: LOGIN_START })
-  axios.post("http://localhost:5000/api/login", credentials)
-  .then(response => {
-    dispatch({ type: LOGIN_SUCCESS,  })
-  })
-  .catch()
-}
+  dispatch({ type: LOGIN_START });
+  axios
+    .post("http://localhost:5000/api/login", credentials)
+    .then(response => {
+      localStorage.setItem("token", response.data.payload);
+      dispatch({ type: LOGIN_SUCCESS, payload: response.data.payload });
+      history.push("/protected")
+    })
+    .catch();
+};
--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,18 +1,24 @@
-
-import { LOGIN_START } from "../actions";
+import { LOGIN_START, LOGIN_SUCCESS } from "../actions";
 
 const initialState = {
   friends: [],
-  error: '',
-  loggingIn: false
+  error: "",
+  loggingIn: false,
+  token: localStorage.getItem("token")
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case LOGIN_START: 
+    case LOGIN_START:
       return {
         ...state,
-        loggingIn: true,
+        loggingIn: true
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loggingIn: false,
+        token: action.payload
       }
     default:
       return state;
