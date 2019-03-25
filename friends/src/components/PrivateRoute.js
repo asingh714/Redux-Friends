194c5ff7552d3c93a743db2ca1b0fb647421879a
Aman <amsingh714@gmail.com>
2019-03-25 19:01:20 -0400

    added errorStatusCode to PrivateRoute

--- friends/src/components/PrivateRoute.js
+++ friends/src/components/PrivateRoute.js
@@ -2,21 +2,30 @@ import React from "react";
 import { Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 
-
-const PrivateRoute = ({ component: Component, token, ...rest }) => {
+const PrivateRoute = ({
+  component: Component,
+  token,
+  errorStatusCode,
+  ...rest
+}) => {
   return (
     <Route
       {...rest}
       render={props =>
-        token ? <Component {...props} /> : <Redirect to="/login" />
+        token && !errorStatusCode === 403 ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
       }
     />
   );
 };
 
-const mapStateToProps = ({ token }) => ({
-  token
-})
+const mapStateToProps = ({ token, errorStatusCode }) => ({
+  token,
+  errorStatusCode
+});
 
 export default withRouter(
   connect(
